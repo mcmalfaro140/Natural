2ceacab864abc09df2f415dde22a28ca53538f87
Veronica Toriz <vtoriz@calstatela.edu>
2020-03-20 12:45:40 -0700

    imgs dynamically uploading

--- src/assets/pages/Menu.js
+++ src/assets/pages/Menu.js
@@ -2,18 +2,24 @@ import React from 'react';
 //import FlatList from 'flatlist-react';
 //import {List} from 'react-native-elements';
 let foodMenu = [{food:"Breakfast"},{ food: "From the Grill"}, {food:"Salads"},{food:"Wraps"},{food: "Bowls"},{food:"Drinks"},];
-let Breakfast = [{srcs: './images/chickenWrap.jpg',name: "Breakfast Sandwich", ingredients: "Eggs, parmesan cheese, tomato, basil, wheat bread", price: "$10.50 + tax"},
-                 {srcs: './images/chickenWrap.jpg',name: "Lunch Sandwich", ingredients: "Eggs, parmesan cheese, tomato, basil,wheat bread", price: "$11.50 + tax"},
-                 {srcs: './images/chickenWrap.jpg',name: "Dinner Sandwich", ingredients: "Eggs, parmesan cheese, tomato, basil,wheat bread", price: "$12.50 + tax"},
-                 {srcs: './images/chickenWrap.jpg',name: "Breakfast Sandwich", ingredients: "Eggs, parmesan cheese, tomato, basil, wheat bread", price: "$10.50 + tax"},
-                 {srcs: './images/chickenWrap.jpg',name: "Lunch Sandwich", ingredients: "Eggs, parmesan cheese, tomato, basil,wheat bread", price: "$11.50 + tax"},
-                 {srcs: './images/chickenWrap.jpg',name: "Dinner Sandwich", ingredients: "Eggs, parmesan cheese, tomato, basil,v", price: "$12.50 + tax"}];
-
 
 
 
 class Menu extends React.Component {
-
+    constructor(props){
+        super(props);
+        this.state={
+            breakfast :[
+                {path: "1",name: "Breakfast Sandwich", ingredients: "Eggs, parmesan cheese, tomato, basil, wheat bread", price: "$10.50 + tax"},
+                {path: "2",name: "Lunch Sandwich", ingredients: "Eggs, parmesan cheese, tomato, basil,wheat bread", price: "$11.50 + tax"},
+                {path: "3",name: "Dinner Sandwich", ingredients: "Eggs, parmesan cheese, tomato, basil,wheat bread", price: "$12.50 + tax"},
+                {path: "4",name: "Breakfast Sandwich", ingredients: "Eggs, parmesan cheese, tomato, basil, wheat bread", price: "$10.50 + tax"},
+                {path: "5",name: "Lunch Sandwich", ingredients: "Eggs, parmesan cheese, tomato, basil,wheat bread", price: "$11.50 + tax"},
+                {path: "6",name: "Dinner Sandwich", ingredients: "Eggs, parmesan cheese, tomato, basil,v", price: "$12.50 + tax"}
+            ]
+        }
+    
+    }
     render(){
 
        const item = foodMenu.map((e, index) => {
@@ -24,20 +30,17 @@ class Menu extends React.Component {
            )
         })
 
-        const dishes = Breakfast.map((e, index) =>{
+        const dishes = this.state.breakfast.map((item) =>{
             return (
                 <div className="img">
-                    <img  src={require('../images/chickenWrap.jpg')}/>
-                    <ul>{e.name}</ul>
-                    <ul>{e.ingredients}</ul>
-                    <ul>{e.price}</ul>
+                    <img  src={require(`../images/breakfast${item.path}.jpg`)}/>
+                    <ul>{item.name}</ul>
+                    <ul>{item.ingredients}</ul>
+                    <ul>{item.price}</ul>
                 </div>
             )
         })
 
-        
-
-        
         return(
             <div  className="menu" >
                 <h1>Menu</h1>

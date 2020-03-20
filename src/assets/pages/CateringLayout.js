8fb026845c7380abf5c15e51a33901bc053f5b53
mcmalfaro140 <mcmalfaro@hotmail.com>
2020-03-20 14:01:12 -0700

    cater img

--- src/assets/pages/CateringLayout.js
+++ src/assets/pages/CateringLayout.js
@@ -8,22 +8,26 @@ class CateringLayout extends React.Component {
                 {
                     title:"Wrap Tray",
                     desc: "Six wraps of your choice cut on fourths. Serves eight to ten people.",
-                    price: "54.95"
+                    price: "54.95",
+                    path: "cater1"
                 },
                 {
                     title:"Sandwich Tray",
                     desc: "Six sandwiches or burgers cut in half. Serves eight to ten people.",
-                    price: "54.95"
+                    price: "54.95",
+                    path: "cater1"
                 },
                 {
                     title:"Salad Bowl",
                     desc: "Your choice of any of our salads. Serves eight to ten people. **ADD Protein for $8.00",
-                    price: "30.95"
+                    price: "30.95",
+                    path: "cater1"
                 },
                 {
                     title:"Fruit Tray",
                     desc: "Your choice of any of our salads. Serves eight to ten people.",
-                    price: "24.95"
+                    price: "24.95",
+                    path: "cater1"
                 },
             ]
         }
@@ -33,6 +37,7 @@ class CateringLayout extends React.Component {
         const plates = this.state.cater.map((item, index) =>{
             return (
                 <div>
+                    <img src={require(`../../assets/images/${item.path}.jpg`)}alt="cater_img"></img>
                     <div>{item.title}</div>
                     <div>{item.desc}</div>
                     <div><span>${item.price}</span> + tax.</div>
